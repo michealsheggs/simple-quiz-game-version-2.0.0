869e0ca8c1ba9ee4ca6b644cbe3840337086c67b
Iluore Emmanuel <michealsheggs@gmail.com>
2019-10-14 09:22:08 +0100

    added dynamic user dropdown menu

--- public/Admin/js/view_quest.js
+++ public/Admin/js/view_quest.js
@@ -1,26 +1,81 @@
-//display the questions and options in the frontpage
- 
-$.getJSON('http://localhost:3000/questions', function(data){
-    $.each(data, function(key, value){  
-      const questionTemplate = $("#displayQuestion").html()
-           
-        $("#Thequestionsection").append(Mustache.render(questionTemplate, value));
-    });
-   
-}
-);
-   
 
-//display the questions on the admin page
-$.getJSON('http://localhost:3000/questions', function(quizQuestions){
-  $.each(quizQuestions, function(key,value){
-    const populateData = $("#templatetable").html()
-        
-      $(".contentTable").append(Mustache.render(populateData, value));
+const setdata =   $.ajax({
+  url: "http://localhost:3000/questions",
+  type: "GET",
+  global: false,
+  async: false,
+  success: function(getdata){
+      return getdata;
+  },   
+}).responseJSON;
 
-    });
+let totalrecord = setdata.length;
+let page = 1;
+let	pagelimit = 4;
 
+fetchData();
+
+// handling the prev-btn
+$("#prevPage").on("click", function(){
+  if(page==1){
+      $("prevPage").addAttr('disabled');
+  }else
+  if (page > 1) {
+      page--;
+      fetchData();
+  }
+  console.log("Prev Page: " + page);
 });
 
+// handling the next-btn
+$("#nextPage").on("click", function(){
+  if (page * pagelimit < totalrecord) {
+      page++;
+      fetchData();
+  }
+  console.log("Next Page: " + page);
+});
+
+function fetchData() {
+  // ajax() method to make api calls
+  $.ajax({
+      url: "http://localhost:3000/questions",
+      type: "GET",
+      data: {
+          _page: page,
+          _limit: pagelimit
+      },
+      success: function(dataArr) {
+          console.log(dataArr);
+
+          if (dataArr) {
+              const totalFetched = dataArr.length;
+              console.log(totalFetched);
+              
+              let fetchData = "";           
+              dataArr.forEach((item)=>{
+                fetchData += `
+                <tr>
+                <th scope="row">${item.id}</th>
+                <td>${item.added_question}</td>
+                <td data-id="to">
+
+                    <button id="edit" data-id="${item.id}" class="btn btn-xs" >pen</button>
+                    <button id="remove" data-id="${item.id}" class="btn btn-xs" >X</button>                             
+                </td>
+                </tr>
+                
+                `;
+                  
+              });
+              
+              $(".contentTable").html(fetchData);
+          }
+      },
+      error: function(err) {
+          return err;
+      }
+  });
+}
 
 
---
+++ public/frontend/pagination.js
@@ -0,0 +1,95 @@
+
+    const setdata =   $.ajax({
+        url: "http://localhost:3000/questions",
+        type: "GET",
+        global: false,
+        async: false,
+        success: function(getdata){
+            return getdata;
+        },   
+    }).responseJSON;
+
+let totalrecord = setdata.length;
+let page = 1;
+let	pagelimit = 4;
+
+    fetchData();
+
+    // handling the prev-btn
+    $("#prevPage").on("click", function(){
+        if(page==1){
+            $("prevPage").addAttr('disabled');
+        }else
+        if (page > 1) {
+            page--;
+            fetchData();
+        }
+        // console.log("Prev Page: " + page);
+    });
+
+    // handling the next-btn
+    $("#nextPage").on("click", function(){
+        if (page * pagelimit < totalrecord) {
+            page++;
+            fetchData();
+        }
+        // console.log("Next Page: " + page);
+    });
+
+    function fetchData() {
+        // ajax() method to make api calls
+        $.ajax({
+            url: "http://localhost:3000/questions",
+            type: "GET",
+            data: {
+                _page: page,
+                _limit: pagelimit
+            },
+            success: function(dataArr) {
+                console.log(dataArr);
+
+                if (dataArr) {
+                    // const totalFetched = dataArr.length;
+                    // console.log(totalFetched);
+                    
+                    let fetchData = "";
+                 
+                    dataArr.forEach((item)=>{
+                      fetchData += `<p class="question text-primary">question ${item.id}</p>
+                        <h4> ${item.added_question}</h4><br/> 
+                        <div class="option">
+                        <div class='form-check'>
+                        <input class='form-check-input' type="radio" name="optiontype${item.id}" id="optiontype" 
+                        value="A">${item.option_one}
+
+                            </div>
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" name="optiontype${item.id}" id="optiontype" 
+                            value="B">${item.option_two}
+                        
+                            </div>
+                            
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" name="optiontype${item.id}" id="optiontype" 
+                            value="C">${item.option_three}
+                                
+                            </div>
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" name="optiontype${item.id}" id="optiontype" 
+                            value="D">${item.option_four}
+                                
+                            </div>            
+                        </div>`;
+
+                    });
+                    
+                    $("#Thequestionsection").html(fetchData);
+                }
+            },
+            error: function(err) {
+                return err;
+            }
+        });
+    }
+
+
